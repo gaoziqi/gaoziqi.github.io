6b5b23d2257b1febd2f0113eff76183acb40a389
gaoziqi <571051761@qq.com>
2017-01-25 06:01:44 -0600

    Update bbwy.js

--- js/bbwy.js
+++ js/bbwy.js
@@ -162,8 +162,8 @@ function animate() {
 function _update() {
 	// delta = change in time since last call (in seconds)
 	var delta = clock.getDelta();
-	//global_is_pc ? controls.update() : Devices.update();
-        if(global_is_pc)cpntrols.update();
+	global_is_pc ? controls.update() : Devices.update();
+        //if(global_is_pc)cpntrols.update();
 }
 
 function _render() {
